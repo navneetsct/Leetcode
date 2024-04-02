334ad24f1d11a8fc7b8d999e1e60c71ab70666eb
navaneet <140655501+navneetsct@users.noreply.github.com>
2024-04-02 11:32:35 +0530

    Create file

---
+++ 338-CountingBits/338-CountingBits.js
@@ -0,0 +1,16 @@
+    }
+    
+    return bitCounts;
+};
+
+
+// Example 1
+const n1 = 2;
+const output1 = countBits(n1);
+console.log(output1); // Output: ["0", "1", "10"]
+
+// Example 2
+const n2 = 5;
+const output2 = countBits(n2);
+console.log(output2); // Output: ["0", "1", "10", "11", "100", "101"]
+2
