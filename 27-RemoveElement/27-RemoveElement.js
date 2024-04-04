75e72b7cacebc6109d1ca58ecd9d7207d8bfe486
navaneet <140655501+navneetsct@users.noreply.github.com>
2024-04-04 09:39:30 +0530

    Create file

---
+++ 27-RemoveElement/27-RemoveElement.js
@@ -0,0 +1,17 @@
+var removeElement = function(nums, val) {
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!==val){
+           nums[index] =nums[i]
+        }
+    }
+  
+return index
+           index++
+    let index=0;
+};
+ */
+ * @return {number}
+ * @param {number} val
+ * @param {number[]} nums
+/**
+[
