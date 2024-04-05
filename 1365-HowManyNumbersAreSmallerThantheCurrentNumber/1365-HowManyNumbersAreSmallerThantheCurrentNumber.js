0edd6eeb9e91286e814a6af8a02382247edd3d4e
navaneet <140655501+navneetsct@users.noreply.github.com>
2024-04-05 10:44:01 +0530

    Create file

---
+++ 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.js
@@ -0,0 +1,15 @@
+var smallerNumbersThanCurrent = function(nums) {
+    let countArr=[];
+
+        for(let i=0;i<nums.length;i++){
+            let count=0;
+          }
+                for(let j=0;j<nums.length;j++){
+                    if(nums[i]>nums[j]){
+                }
+                        count++
+                    }
+      return countArr;
+                countArr.push(count)
+};
+[
