7ab5bacc122bd4d175f2e4f75054af980d6a5677
navaneet <140655501+navneetsct@users.noreply.github.com>
2024-04-06 11:57:47 +0530

    Create file

---
+++ 1221-SplitaStringinBalancedStrings/1221-SplitaStringinBalancedStrings.js
@@ -0,0 +1,17 @@
+
+   let matches=0;
+   let balances=0;
+
+   for(let i=0;i<s.length;i++){
+    if(s[i]==='R'){
+        balances-= 1;
+    }else if
+        (s[i]==='L'){
+    }if(balances===0){
+            balances+=1
+        matches+=1
+    }
+   } 
+   return matches
+};
+"
