b087cdb8caf75ec098d1ba9a0ae29706f80519dc
navaneet <140655501+navneetsct@users.noreply.github.com>
2024-04-08 01:15:12 +0530

    Create file

---
+++ 13-RomantoInteger/13-RomantoInteger.js
@@ -0,0 +1,17 @@
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const cur = sym[s[i]];
+        const next = sym[s[i + 1]];
+
+        if (cur < next) {
+            result += next - cur;
+            i++;
+        } else {
+            result += cur;
+        }
+    }
+
+    return result;
+};
+"
