47f9892a9327abd0079dfb1b01c2d1af40cdf79c
navaneet <140655501+navneetsct@users.noreply.github.com>
2024-04-17 10:03:55 +0530

    Create file

---
+++ 2535-DifferenceBetweenElementSumandDigitSumofanArray/2535-DifferenceBetweenElementSumandDigitSumofanArray.js
@@ -0,0 +1,16 @@
+    let esum=0;
+
+        for(let i=0;i<nums.length;i++){
+            let s=nums[i].toString()
+        }
+            esum=esum+nums[i];
+                for(let j=0;j<s.length;j++){
+                    dsum=dsum+parseInt(s[j])
+                }
+
+    
+        let res=esum-dsum;
+        return Math.abs(res)
+};
+    let dsum=0;
+[
