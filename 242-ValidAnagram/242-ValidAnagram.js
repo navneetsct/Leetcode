bae4747c6e24538471ae0798f20d069c701af9c9
navaneet <140655501+navneetsct@users.noreply.github.com>
2024-04-17 11:34:13 +0530

    Create file

---
+++ 242-ValidAnagram/242-ValidAnagram.js
@@ -0,0 +1,17 @@
+        if(s.length!==t.length)
+            return false;
+        
+    for(let i=0;i<s.length;i++)
+        if(s[i]!==t[i])
+           
+    
+            return false;
+        
+            return true;
+        
+
+    t=t.split("").sort()
+
+};
+
+"
